e5330cc6d76ed2e8bc113dee6e010d2c5544d071
Arto Levi <arto.levi@tuta.com>
2026-04-23 00:26:47 +0300

    change nu config

--- modules/home/pkgs/nushell/default.nix
+++ modules/home/pkgs/nushell/default.nix
@@ -4,26 +4,14 @@
     enable = true;
 
     shellAliases = {
-      # ls replacements (eza)
-      ls = "eza";
-      ll = "eza -l --git";
-      la = "eza -la --git";
-      lt = "eza --tree --level=2";
-
-      # Editor
-      e = "zeditor";
-
-      # File viewing
-      cat = "bat";
-
       # Git shortcuts
       gs = "git status";
       gd = "git diff";
       gl = "git log --oneline --graph --decorate -20";
 
       # Flake shortcuts
-      flake-update = "nix flake update";
-      flake-check = "nix flake check";
+      nfu = "nix flake update";
+      nfc = "nix flake check";
 
       # Nix shells
       nd = "nix develop -c nu";
